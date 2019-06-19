5c56c1570b2125271b1090ce6cf9c5d14f248315
cassinodari <cassi.nodari@hotmail.com>
2019-06-18 22:16:34 -0300

    Controla led vermelho e verde

--- App.js
+++ App.js
@@ -1,49 +1,200 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {
+  StyleSheet, Text, View, Button, FlatList,
+  Switch, TouchableOpacity, ToastAndroid
+} from 'react-native';
+import BluetoothSerial from 'react-native-bluetooth-serial'
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    this.state = {
+      isEnabled: false,
+      discovering: false,
+      devices: [],
+      unpairedDevices: [],
+      connected: false,
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  componentWillMount() {
+    Promise.all([
+      BluetoothSerial.isEnabled(),
+      BluetoothSerial.list()
+    ])
+      .then((values) => {
+        const [isEnabled, devices] = values
+        this.setState({ isEnabled, devices })
+      });
+
+    BluetoothSerial.on('bluetoothEnabled', () => {
+      Promise.all([
+        BluetoothSerial.isEnabled(),
+        BluetoothSerial.list()
+      ])
+        .then((values) => {
+          const [devices] = values
+          this.setState({ devices })
+        });
+
+      BluetoothSerial.on('bluetoothDisabled', () => {
+        this.setState({ devices: [] })
+      });
+
+      BluetoothSerial.on('error', (err) => console.log(`Error: ${err.message}`))
+    });
+  }
+
+  toggleBluetooth(value) {
+    if (value === true) {
+      this.enable()
+    } else {
+      this.disable()
+    }
+  }
+
+  enable() {
+    BluetoothSerial.enable()
+      .then((res) => this.setState({ isEnabled: true }))
+      .catch((err) => ToastAndroid.showShortBottom(err.message))
+  }
+
+  disable() {
+    BluetoothSerial.disable()
+      .then((res) => this.setState({ isEnabled: false }))
+      .catch((err) => ToastAndroid.showShortBottom(err.message))
+  }
+
+  discoverAvailableDevices() {
+    if (this.state.discovering) {
+      return false;
+    }
+    else {
+      this.setState({ discovering: true });
+
+      BluetoothSerial.discoverUnpairedDevices()
+        .then((unpairedDevices) => {
+          const uniqueDevices = _.uniqBy(unpairedDevices, 'id');
+          console.log(uniqueDevices);
+          this.setState({ unpairedDevices: uniqueDevices, discovering: false })
+        })
+        .catch((err) => console.log(err.message))
+    }
+  }
+
+  activeGreenLed() {
+    BluetoothSerial.write("0")
+      .then((res) => {
+        console.log(res);
+        console.log('Successfuly wrote to device')
+        this.setState({ connected: true })
+      })
+      .catch((err) => console.log(err.message))
+  }
+
+  activeRedLed() {
+    BluetoothSerial.write("1")
+      .then((res) => {
+        console.log(res);
+        console.log('Successfuly wrote to device')
+        this.setState({ connected: true })
+      })
+      .catch((err) => console.log(err.message))
+  }
+
+  connect(device) {
+    this.setState({ connected: true });
+
+    BluetoothSerial.connect(device.id)
+      .then((res) => {
+        console.log(`Connected to device ${device.name}`);
+        ToastAndroid.show(`Connected to device ${device.name}`, ToastAndroid.SHORT);
+      })
+      .catch((err) => console.log((err.message)))
+  }
+
+  renderDevices(item) {
+    return (
+      <TouchableOpacity onPress={() => this.connect(item.item)}>
+        <View style={styles.deviceNameWrap}>
+          <Text style={styles.deviceName}>
+            {item.item.name ? item.item.name : item.item.id}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.toolbar}>
+          <Text style={styles.toolbarTitle}>Bluetooth Device List</Text>
+          <View style={styles.toolbarButton}>
+            <Switch
+              value={this.state.isEnabled}
+              onValueChange={(val) => this.toggleBluetooth(val)}
+            />
+          </View>
+        </View>
+
+        <Button
+          onPress={this.discoverAvailableDevices.bind(this)}
+          title="Scan for Devices"
+          color="#841584"
+        />
+
+        <FlatList
+          style={{ flex: 1 }}
+          data={this.state.devices}
+          keyExtractor={item => item.id}
+          renderItem={(item) => this.renderDevices(item)}
+        />
+
+        <Button
+          onPress={this.activeGreenLed.bind(this)}
+          title="Green"
+          color="#287731"
+        />
+
+        <Button
+          onPress={this.activeRedLed.bind(this)}
+          title="Red"
+          color="#aa3443"
+        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  toolbar: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    flexDirection: 'row'
+  },
+  toolbarButton: {
+    width: 50,
+    marginTop: 8,
+  },
+  toolbarTitle: {
+    textAlign: 'center',
+    fontWeight: 'bold',
     fontSize: 20,
-    textAlign: 'center',
+    flex: 1,
+    marginTop: 6
+  },
+  deviceName: {
+    fontSize: 17,
+    color: "black"
+  },
+  deviceNameWrap: {
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    borderBottomWidth: 1
+  }
 });
